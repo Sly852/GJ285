819b8b3b67cf89d70528542262acc502048da3af
Sly852 <rachataounsamer852@gmail.com>
2023-10-18 04:00:06 +0700

    Added Boss

--- GJ_2D/Assets/Script/PlayerController.cs
+++ GJ_2D/Assets/Script/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D playerCollider;
 
+    public Animator animator;
+    public Transform attackPoint;
+    public LayerMask bulletLayers;
     public float jumpForce = 10.5f;
+    public float attackRange = 0.5f;
 
     private GameObject currentPlatform;
-    private bool canJump = true;
     private Rigidbody2D rb;
+    private bool canJump = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +25,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Attack();
+        }
         if (canJump && Input.GetKeyDown(KeyCode.Z))
         {
             Jump();
@@ -33,6 +42,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Attack()
+    {
+        //Play an attack animation
+        animator.SetTrigger("Attack");
+
+        //Detect enmies in range of attack
+        Collider2D[] hitBullet = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, bulletLayers);
+    
+        foreach(Collider2D bullet in hitBullet)
+        {
+            Debug.Log("We hit" + bullet.name);
+        }
+    }
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
@@ -46,7 +68,7 @@ public class PlayerController : MonoBehaviour
         BoxCollider2D platformCollider = currentPlatform.GetComponent<BoxCollider2D>();
 
         Physics2D.IgnoreCollision(playerCollider, platformCollider);
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(1.0f);
         Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
     }
     private void Jump()
